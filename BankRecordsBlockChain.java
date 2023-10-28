41cdcd8a18297a4c83f6272e7672b7415e9d75ec
kas7927 <143525287+kas7927@users.noreply.github.com>
2023-10-27 21:01:37 -0400

    BankTransactions added StudentGrades added

--- BankRecordsBlockChain.java
+++ BankRecordsBlockChain.java
@@ -6,12 +6,17 @@ public class BankRecordsBlockChain {
         public BankRecordsBlockChain() {
             chain = new ArrayList<BankRecords>();
             // Create the genesis block (the first block in the chain)
-            chain.add(new BankRecords(0, "0", "Genesis Block", 123, "Savings", "Debit", 200));
+            chain.add(new BankRecords(0, "0", "Student 1", 123, "Savings", "Debit", 200));
         }
         public void addBlock(String data) {
             BankRecords previousBlock = chain.get(chain.size() - 1);
             BankRecords newBlock = new BankRecords(previousBlock.getIndex() + 1, previousBlock.getHash(), data, 12345, "Savings", "Debit", 700);
             chain.add(newBlock);
+        }
+        public void addBlock2(String data) {
+            BankRecords previousBlock = chain.get(chain.size() - 1);
+            BankRecords newBlock1 = new BankRecords(previousBlock.getIndex() + 1, previousBlock.getHash(), data,123455, "Checking", "Debit", 550);
+            chain.add(newBlock1);
         }
         public void printBlockchain() {
             for (BankRecords bankRecords : chain) {
--- SimpleBlockchain.java
+++ SimpleBlockchain.java
@@ -1,17 +1,28 @@
 public class SimpleBlockchain {
     public static void main(String[] args) {
+
         // Create a new blockchain
         Blockchain blockchain = new Blockchain();
+
         // Add some blocks to the blockchain
         blockchain.addBlock("Transaction 1");
         blockchain.addBlock("Transaction 2");
         blockchain.addBlock("Transaction 3");
+
         // Print the blockchain
         blockchain.printBlockchain();
 
         BankRecordsBlockChain blockChain1 = new BankRecordsBlockChain();
-        blockChain1.addBlock("Transaction 4");
+        blockChain1.addBlock("Transaction 2");
+        blockChain1.addBlock2("Transaction 3");
 
         blockChain1.printBlockchain();
+
+        StudentGradesBlockChain blockChain2 = new StudentGradesBlockChain();
+        blockChain2.addBlock("Student 2");
+        blockChain2.addBlock2("Student 3");
+
+        blockChain2.printBlockChain();
+
     }
 }
